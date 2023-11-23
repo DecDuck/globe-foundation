5e3a761eb5952f438f4dc1c6add79cf290457914
DecDuck <declanahofmeyr@gmail.com>
2023-11-23 22:32:07 +1100

    updated base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: "/globe-foundation/",
+  },
   modules: ["@nuxtjs/tailwindcss"],
   vite: {
     plugins: [nodePolyfills()],
